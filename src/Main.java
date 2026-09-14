359138c2ad42bc520d3ddbe38e1b214c6d52afb7
Sergiusz <mazursergiusz0@gmail.com>
2026-09-15 00:16:08 +0200

    Zadanie 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 void main() {
 
 
@@ -19,7 +20,13 @@ void main() {
             System.out.println("Mam na imię " + imie + ", mam " + wiek
                 + " lat i będę pisać maturę za " + liczba + " roku.");
 
+            // Zadanie 4
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.print("Podaj temperaturę w Celsjuszach: ");
+            double stopnie = scanner.nextDouble();
+            double fahrenheit = 1.8 * stopnie + 32.0;
+            System.out.println("Temperatura w Fahrenheitach: " + fahrenheit);
 
 
 
